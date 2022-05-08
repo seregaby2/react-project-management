96cfc1d44ff14d707678f67b7bc21440a002fa3a
Sergey Kruptsov <serega.by@inbox.ru>
2022-05-09 00:02:16 +0300

    fix: fix structure login page

--- my-app/src/components/Router.tsx
+++ my-app/src/components/Router.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { BoardPage, HomePage, LoginPage, MainPage, NotFoundPage, ProfilePage } from '../pages';
 
-// TODO Remove after changing!
-import { FormAuthPage } from '../pages/formForLogIn/formLogin';
-
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/main" element={<MainPage />} />
       <Route path="/board" element={<BoardPage />} />
-      <Route path="/login" element={<FormAuthPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/profile" element={<ProfilePage />} />
       {/*<Route path="/login" element={<LoginPage />} />*/}
       <Route path="*" element={<NotFoundPage />} />
--- my-app/src/pages/LoginPage/LoginPage.tsx
+++ my-app/src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,121 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import { Button, TextField } from '@mui/material';
+import { useForm, Controller, SubmitHandler, useFormState } from 'react-hook-form';
+import {
+  loginValidation,
+  nameValidation,
+  passwordValidation,
+} from '../../components/validation/validation';
 import styles from './LoginPage.module.scss';
 
-export const LoginPage = () => {
-  return <main className={styles.loginPage}>LoginPage</main>;
-};
+interface ISignInForm {
+  name: string;
+  login: string;
+  password: string;
+}
+
+export function LoginPage() {
+  const { handleSubmit, control } = useForm<ISignInForm>();
+  const onSubmit: SubmitHandler<ISignInForm> = (data) => console.log(data);
+  const { errors } = useFormState({
+    control,
+  });
+
+  return (
+    <div className={styles.formAuthPage}>
+      <Typography variant="h4" component="div">
+        Sign up
+      </Typography>
+      <Typography
+        variant="subtitle1"
+        component="div"
+        gutterBottom={true}
+        className={styles.formAuthPageSubtitle}
+      >
+        to get access
+      </Typography>
+      <form className={styles.formAuthPageForm} onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="name"
+          rules={nameValidation}
+          render={({ field }) => (
+            <TextField
+              label="name"
+              size="small"
+              margin="normal"
+              fullWidth
+              onChange={(e) => field.onChange(e)}
+              value={field.value || ''}
+              error={!!errors.name?.message}
+              helperText={errors.name?.message}
+              FormHelperTextProps={{
+                style: {
+                  position: 'absolute',
+                  top: '35px',
+                },
+              }}
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="login"
+          rules={loginValidation}
+          render={({ field }) => (
+            <TextField
+              label="login"
+              size="small"
+              margin="normal"
+              fullWidth
+              onChange={(e) => field.onChange(e)}
+              value={field.value || ''}
+              error={!!errors.login?.message}
+              helperText={errors.login?.message}
+              FormHelperTextProps={{
+                style: {
+                  position: 'absolute',
+                  top: '35px',
+                },
+              }}
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="password"
+          rules={passwordValidation}
+          render={({ field }) => (
+            <TextField
+              type="password"
+              label="password"
+              size="small"
+              margin="normal"
+              fullWidth
+              onChange={(e) => field.onChange(e)}
+              error={!!errors.password?.message}
+              helperText={errors.password?.message}
+              FormHelperTextProps={{
+                style: {
+                  position: 'absolute',
+                  top: '35px',
+                },
+              }}
+            />
+          )}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          fullWidth
+          sx={{
+            marginTop: 2,
+          }}
+        >
+          register
+        </Button>
+      </form>
+    </div>
+  );
+}
