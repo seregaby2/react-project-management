24a6169ff4a3c137fcf2ef50b832f233a29ea687
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-05-14 13:40:58 +0300

    feat: add global search

---
+++ my-app/src/components/MainFilter/MainFilter.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import styles from './MainFilter.module.scss';
+import { useAppSelector } from '../../hooks/redux';
+import { FormControl, FormGroup, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { IMainFilterProps } from './IMainFilterProps';
+type FilterType = 'title' | 'description' | 'workers';
+
+const MainFilter = ({ inputHandler }: IMainFilterProps) => {
+  const { boards } = useAppSelector((state) => state.reducerBoards);
+  const [filter, setFilter] = useState<FilterType>('title');
+  useEffect(() => {
+    inputHandler(boards);
+  }, []);
+  const searchHandler = (value: string) => {
+    inputHandler(boards.filter((board) => board[filter].includes(value)));
+  };
+  return (
+    <>
+      <FormGroup row className={styles.filterGroup}>
+        <FormControl fullWidth className={styles.filterInput}>
+          <TextField
+            placeholder="Search..."
+            type="text"
+            onChange={(e) => searchHandler(e.target.value)}
+          />
+        </FormControl>
+        <FormControl className={styles.filterSelect}>
+          <InputLabel>Filter</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            value={filter}
+            label="Filter"
+            onChange={(e) => setFilter(e.target.value as FilterType)}
+          >
+            <MenuItem value={'title'}>Title</MenuItem>
+            <MenuItem value={'description'}>Description</MenuItem>
+            <MenuItem value={'workers'}>Workers</MenuItem>
+          </Select>
+        </FormControl>
+      </FormGroup>
+    </>
+  );
+};
+
+export default MainFilter;
--- my-app/src/pages/MainPage/MainPage.tsx
+++ my-app/src/pages/MainPage/MainPage.tsx
@@ -3,25 +3,36 @@ import styles from './MainPage.module.scss';
 import { useAppSelector } from '../../hooks/redux';
 import { Board } from '../../components';
 import { CreateBoardForm } from '../../components/CreateBoardForm/CreateBoardForm';
+import MainFilter from '../../components/MainFilter/MainFilter';
+import { Button, Typography } from '@mui/material';
+import { IBoard } from '../../interfaces/IBoard';
 
 export const MainPage = () => {
-  const { boards } = useAppSelector((state) => state.reducerBoards);
+  const [visibleBoards, setVisibleBoards] = useState<IBoard[]>([]);
   const [showCreateBoardForm, setShowCreateBoardForm] = useState<boolean>(false);
   return (
     <main className={styles.mainPage}>
       <div className={styles.containerForInputAndButton}>
-        <input type="text" />
-        <button onClick={() => setShowCreateBoardForm(true)}>Create board</button>
+        <MainFilter inputHandler={(boards: IBoard[]) => setVisibleBoards(boards)} />
+        <Button className={styles.createBoardBtn} onClick={() => setShowCreateBoardForm(true)}>
+          Create board
+        </Button>
       </div>
       {showCreateBoardForm && (
         <CreateBoardForm hideCreateBoardForm={() => setShowCreateBoardForm(false)} />
       )}
       <ul>
-        {boards.map((board) => (
-          <li key={board.id}>
-            <Board id={board.id} title={board.title} description={board.description} />
-          </li>
-        ))}
+        {visibleBoards.length !== 0 ? (
+          visibleBoards.map((board) => (
+            <li key={board.id}>
+              <Board id={board.id} title={board.title} description={board.description} />
+            </li>
+          ))
+        ) : (
+          <Typography variant="h2" component="div">
+            No boards=(
+          </Typography>
+        )}
       </ul>
     </main>
   );
