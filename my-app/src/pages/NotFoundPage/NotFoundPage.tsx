c725ed745b298bcb2caf5dcc3ac8b400ac1df3e3
Sergey Kruptsov <serega.by@inbox.ru>
2022-05-24 11:56:42 +0300

    feat: PM 11 add translation not found page

--- my-app/src/pages/NotFoundPage/NotFoundPage.tsx
+++ my-app/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import styles from './NotFoundPage.module.scss';
 import notFoundImg from '../../assets/img/not_found.png';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 export function NotFoundPage() {
+  const { t } = useTranslation(['notFoundPage']);
+
   return (
     <main className={styles.notFoundPage}>
-      <p>Something went wrong...</p>
+      <p>{t('wrong')}</p>
       <img src={notFoundImg} alt="not_found" />
       <p>
-        This page doesn<span>&#39;</span>t exist. Go to <Link to="/">Home</Link>
+        {t('notFound')} <Link to="/">{t('home')}</Link>
       </p>
     </main>
   );
